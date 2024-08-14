dca14728e5ea8b44613ed5d2c712ea8519c05244
achush-hash <achush@umich.edu>
2024-08-14 12:19:47 -0500

    socr ai bot code initial push

--- app.R
+++ app.R
@@ -1,194 +1,28 @@
-library(shiny)
-library(httr)
-library(sass)
-library(markdown)
-library(waiter)
-library(shinyjs)
-library(shinyCopy2clipboard)
-library(reticulate)
-# remotes::install_github("deepanshu88/shinyCopy2clipboard")
+source("ui.R")
+source("server.R")
+source("testing.R")
 
-# virtualenv_install("pyDev", packages = c('llama_index','llama-index-embeddings-huggingface',
-#                                          'llama-index-embeddings-langchain','llama-index-llms-huggingface',
-#                                          'llama-index-llms-openai','llama-index-embeddings-instructor'))
-# use_virtualenv("pyDev", required = TRUE)
-use_condaenv(condaenv = "pytorch_env", required = TRUE)
-py_libs <- import("rag_bot_inference_engine")
+# moved to ./R/data_prep.R
+# ###################################################
+# # Global variables
+# ###################################################
 
-css <- sass(sass_file("www/chat.scss"))
-jscode <- 'var container = document.getElementById("chat-container");
-if (container) {
-  var elements = container.getElementsByClassName("user-message");
-  if (elements.length > 1) {
-    var lastElement = elements[elements.length - 1];
-    lastElement.scrollIntoView({
-      behavior: "smooth"
-    });
-  }
-}'
-
-socrragGPT <-function(prompt) {
-  req <- POST("http://127.0.0.1:5000/generate_response",
-              body = list(prompt = prompt),
-              encode = "json")
-  
-  if (status_code(req) == 200) {
-    response_data <- content(req, "parsed")
-    result <- response_data$response
-  } else {
-    result <- "error"
-  }
-  
-  return(result)
-}
-chatGPT_R <- function(apiKey, prompt, model="gpt-3.5-turbo") {
-  response <- POST(
-    url = "https://api.openai.com/v1/chat/completions",
-    add_headers(Authorization = paste("Bearer", apiKey)),
-    content_type("application/json"),
-    encode = "json",
-    body = list(
-      model = model,
-      messages = list(
-        list(role = "user", content = prompt)
-      )
-    )
-  )
-
-  if(status_code(response)>200) {
-    result <- trimws(content(response)$error$message)
-  } else {
-    result <- trimws(content(response)$choices[[1]]$message$content)
-  }
-
-  return(result)
-
-}
-
-execute_at_next_input <- function(expr, session = getDefaultReactiveDomain()) {
-  observeEvent(once = TRUE, reactiveValuesToList(session$input), {
-    force(expr)
-  }, ignoreInit = TRUE)
-}
-
-# Define UI for application
-ui <- fluidPage(
-  useWaiter(),
-  useShinyjs(),
-  use_copy(),
-  tags$head(tags$style(css)),
-  sidebarLayout(
-    sidebarPanel(
-      textInput("apiKey", "API Key", "sk-xxxxxxxxxxxxxxxxxxxx"),
-      selectInput("model", "Model", choices = c("gpt-3.5-turbo", "gpt-4"), selected = "gpt-3.5-turbo"),
-      style = "background-color: #fff; color: #333; border: 1px solid #ccc;"
-    ),
-
-    mainPanel(
-      tags$div(
-        id = "chat-container",
-        tags$div(
-          id = "chat-header",
-          # tags$img(src = "TnUa864.png", alt = "AI Profile Picture"),
-          tags$h3("SOCR AI Assistant")
-        ),
-        tags$div(
-          id = "chat-history",
-          uiOutput("chatThread"),
-        ),
-        tags$div(
-          id = "chat-input",
-          tags$form(
-            column(12,textAreaInput(inputId = "prompt", label="", placeholder = "Type your message here...", width = "100%")),
-            fluidRow(
-              tags$div(style = "margin-left: 1.5em;",
-                       actionButton(inputId = "submit",
-                                    label = "Send",
-                                    icon = icon("paper-plane")),
-                       actionButton(inputId = "remove_chatThread",
-                                    label = "Clear History",
-                                    icon = icon("trash-can")),
-                       CopyButton("clipbtn",
-                                   label = "Copy",
-                                   icon = icon("clipboard"),
-                                  text = "")
-
-              ))
-          ))
-      )
-    ))
-)
-
-# Define server logic
-server <- function(input, output, session) {
-
-  historyALL <- reactiveValues(df = data.frame() , val = character(0))
-
-  # On click of send button
-  observeEvent(input$submit, {
-
-    if (nchar(trimws(input$prompt)) > 0) {
-
-      # Spinner
-      w <- Waiter$new(id = "chat-history",
-                      html = spin_3(),
-                      color = transparent(.5))
-      w$show()
-
-      # Response
-      # chatGPT <- chatGPT_R(input$apiKey, input$prompt, input$model)
-      # chatGPT<- socrragGPT(input$prompt)
-      chatGPT <- py_libs$generate_response(input$apiKey, input$model,  input$prompt)
-      print(chatGPT)
-      historyALL$val <- chatGPT
-      history <- data.frame(users = c("Human", "AI"),
-                            content = c(input$prompt, markdown::mark_html(text=chatGPT)),
-                            stringsAsFactors = FALSE)
-      historyALL$df <- rbind(historyALL$df, history)
-      updateTextInput(session, "prompt", value = "")
-
-    # Conversation Interface
-    output$chatThread <- renderUI({
-      conversations <- lapply(seq_len(nrow(historyALL$df)), function(x) {
-        tags$div(class = ifelse(historyALL$df[x, "users"] == "Human",
-                                "user-message",
-                                "bot-message"),
-                 HTML(paste0(ifelse(historyALL$df[x, "users"] == "Human",
-                                    "<div class='img-wrapper'></div>",
-                                    "<div class='img-wrapper'><img src='socr_logo.png' class='img-wrapper2'></div>"),
-                             historyALL$df[x, "content"])))
-      })
-      do.call(tagList, conversations)
-    })
-
-    w$hide()
-    execute_at_next_input(runjs(jscode))
-
-    }
-
-  })
-
-  observeEvent(input$remove_chatThread, {
-    output$chatThread <- renderUI({return(NULL)})
-    historyALL$df <- NULL
-    updateTextInput(session, "prompt", value = "")
-  })
-
-  observe({
-    req(input$clipbtn)
-    CopyButtonUpdate(session,
-                     id = "clipbtn",
-                     label = "Copy",
-                     icon = icon("clipboard"),
-                     text = as.character(historyALL$val))
-
-  })
+#############################################################################
+# Per RShiny Instructions, auto-load supplementary functions from ./R folder
+# and HTML content from ./www foldera
+# https://shiny.rstudio.com/articles/app-formats.html
+#
+# data_prep.R
+# support_functions.R
+#############################################################################
 
 
-}
 
 
-# Run the application
-shinyApp(ui=ui, server=server)
+# Run the application 
 
 
+
+app <- shinyApp(ui = app_ui, server = app_server)
+
+runApp()
---
+++ global.R
@@ -0,0 +1,126 @@
+source("R/data_prep.R")
+source("R/palmAI.R")
+source("R/support_functions.R")
+
+library(shiny)
+library(golem)
+library(RCurl)
+library(json64)
+library(plotly)
+# library(biomartr)
+library(shinydashboard)
+library(shinydashboardPlus)
+library(shinythemes)
+library(shinyBS)
+library(reticulate)
+library(httr)
+
+
+#' Add external Resources to the Application
+#'
+#' This function is internally used to add external
+#' resources inside the Shiny application.
+#'
+#' @import shiny
+#' @importFrom golem add_resource_path activate_js favicon bundle_resources
+#' @noRd
+
+golem_add_external_resources <- function() {
+  add_resource_path(
+    "www", app_sys("app/www")
+  )
+  
+  tags$head(
+    favicon(
+      ico = "icon",
+      rel = "shortcut icon",
+      resources_path = "www",
+      ext = "png"
+    ),
+    bundle_resources(
+      path = app_sys("app/www"),
+      app_title = "AI Bot"
+    )
+    # Add here other external resources
+    # for example, you can add shinyalert::useShinyalert()
+  )
+}
+
+#' Access files in the current app
+#'
+#' NOTE: If you manually change your package name in the DESCRIPTION,
+#' don't forget to change it here too, and in the config file.
+#' For a safer name change mechanism, use the `golem::set_golem_name()` function.
+#'
+#' @param ... character vectors, specifying subdirectory and file(s)
+#' within your package. The default, none, returns the root of the app.
+#'
+#' @noRd
+app_sys <- function(...) {
+  system.file(..., package = "AIBot")
+}
+
+
+#' Read App Config
+#'
+#' @param value Value to retrieve from the config file.
+#' @param config GOLEM_CONFIG_ACTIVE value. If unset, R_CONFIG_ACTIVE.
+#' If unset, "default".
+#' @param use_parent Logical, scan the parent directory for config file.
+#' @param file Location of the config file
+#'
+#' @noRd
+get_golem_config <- function(
+    value,
+    config = Sys.getenv(
+      "GOLEM_CONFIG_ACTIVE",
+      Sys.getenv(
+        "R_CONFIG_ACTIVE",
+        "default"
+      )
+    ),
+    use_parent = TRUE,
+    # Modify this if your config file is somewhere else
+    file = app_sys("golem-config.yml")
+) {
+  config::get(
+    value = value,
+    config = config,
+    file = file,
+    use_parent = use_parent
+  )
+}
+
+demo_questions <- c(
+  'Example questions:' = "Example questions:",
+  'Lookup R package' = "List popular R packages for time-series forecast.",
+  'Explain concepts' = "What is Moran's I in spatial statistics?",
+  'Find statistic method' = "What statistics test should I use to examine the 
+  correlation of two categorical variable?",
+  'How to, situation' = "How do you do regression when there are
+predictor variables that are highly correlated?",
+  'How to, methods' = "How do you choose k in k-means clustering?",
+  'How to, evaluation' = "How do you assess linear regression models?",
+  'How to, outliers' = "How to deal with outliers?",
+  'How to, interpretation' = "What does P value = 0.02 mean in ANOVA?",
+  'Vague question, rejected' = "How does k-means clustering work?",
+  'Vague question, w/ context' = "How does k-means clustering work in statistics?"
+)
+
+demo_model_choose <- c('Choose Model: (Default: GPT-3.5 Turbo)',
+                       'GPT-3.5 Turbo',
+                       'Gemini 1.0 Pro')
+
+demo_synth_prompts <- c('Example Text Prompts:' = "Example themes:",
+                        'Medical note' = "Draft a clinical note for an Alzheimer's disease patient",
+                        'Social Essay' = "Expected health disparities and racial inequality in the US in 2030")
+
+img_prompts <- c('Example Image Prompts:' = "Example image prompts:",
+                 'Healthy Control' = "2D sagittal MRI brain image of Healthy Asymptomatic Control",
+                 'Patients' = "2D sagittal MRI brain image of Alzheimer's disease")
+
+
+
+
+
+
